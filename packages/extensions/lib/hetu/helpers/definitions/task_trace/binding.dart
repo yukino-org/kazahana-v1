40e9384cce1c91302df49367cc43bfcd5b89228c
zyrouge <naveencnaveen96@gmail.com>
2022-01-13 16:25:32 +0530

    chore(extensions): fix hetu definitions

--- packages/extensions/lib/hetu/helpers/definitions/task_trace/binding.dart
+++ packages/extensions/lib/hetu/helpers/definitions/task_trace/binding.dart
@@ -1,6 +1,7 @@
 import 'package:hetu_script/binding.dart';
 import 'package:hetu_script/hetu_script.dart';
 import './class.dart';
+import '../../model.dart';
 
 class TaskTraceClassBinding extends HTExternalClass {
   TaskTraceClassBinding() : super('TaskTrace');
@@ -30,20 +31,26 @@ class TaskTraceClassBinding extends HTExternalClass {
     final TaskTrace element = object as TaskTrace;
     switch (varName) {
       case 'add':
-        return ({
-          final List<dynamic> positionalArgs = const <dynamic>[],
-          final Map<String, dynamic> namedArgs = const <String, dynamic>{},
-          final List<HTType> typeArgs = const <HTType>[],
-        }) =>
-            element.add(positionalArgs[0] as String);
+        return createHTExternalFunction(
+          (
+            final HTEntity entity, {
+            final List<dynamic> positionalArgs = const <dynamic>[],
+            final Map<String, dynamic> namedArgs = const <String, dynamic>{},
+            final List<HTType> typeArgs = const <HTType>[],
+          }) =>
+              element.add(positionalArgs[0] as String),
+        );
 
       case 'toString':
-        return ({
-          final List<dynamic> positionalArgs = const <dynamic>[],
-          final Map<String, dynamic> namedArgs = const <String, dynamic>{},
-          final List<HTType> typeArgs = const <HTType>[],
-        }) =>
-            element.toString();
+        return createHTExternalFunction(
+          (
+            final HTEntity entity, {
+            final List<dynamic> positionalArgs = const <dynamic>[],
+            final Map<String, dynamic> namedArgs = const <String, dynamic>{},
+            final List<HTType> typeArgs = const <HTType>[],
+          }) =>
+              element.toString(),
+        );
 
       default:
         throw HTError.undefined(varName);
