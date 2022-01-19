3bf529bff99d1023d06e88909c7bf003a6a0b43b
zyrouge <naveencnaveen96@gmail.com>
2022-01-19 12:30:57 +0530

    chore(extensions): update regex bindings

--- packages/extensions/lib/hetu/helpers/definitions/regex/match/binding.dart
+++ packages/extensions/lib/hetu/helpers/definitions/regex/match/binding.dart
@@ -1,6 +1,7 @@
 import 'package:hetu_script/binding.dart';
 import 'package:hetu_script/hetu_script.dart';
 import './class.dart';
+import '../../../model.dart';
 
 class RegexMatchClassBinding extends HTExternalClass {
   RegexMatchClassBinding() : super('RegexMatch');
@@ -14,12 +15,15 @@ class RegexMatchClassBinding extends HTExternalClass {
         return element.input;
 
       case 'group':
-        return ({
-          final List<dynamic> positionalArgs = const <dynamic>[],
-          final Map<String, dynamic> namedArgs = const <String, dynamic>{},
-          final List<HTType> typeArgs = const <HTType>[],
-        }) =>
-            element.group(positionalArgs.first as int);
+        return createHTExternalFunction(
+          (
+            final HTEntity entity, {
+            final List<dynamic> positionalArgs = const <dynamic>[],
+            final Map<String, dynamic> namedArgs = const <String, dynamic>{},
+            final List<HTType> typeArgs = const <HTType>[],
+          }) =>
+              element.group(positionalArgs.first as int),
+        );
 
       default:
         throw HTError.undefined(varName);
