1bc8b58ae82e387805f565be49b98d1245009f93
zyrouge <naveencnaveen96@gmail.com>
2022-03-22 19:51:54 +0530

    chore: avoid generating locale file

--- cli/commands/generator/run/index.ts
+++ cli/commands/generator/run/index.ts
@@ -1,15 +1,10 @@
 import { Logger } from "../../../logger";
 import { runDartBuildRunner } from "./tasks/build_runner";
-import { generateLocales } from "./tasks/locale";
 import { generateMeta } from "./tasks/meta";
 
 const logger = new Logger("generator:run");
 
-const tasks: (() => Promise<void>)[] = [
-    runDartBuildRunner,
-    generateLocales,
-    generateMeta,
-];
+const tasks: (() => Promise<void>)[] = [runDartBuildRunner, generateMeta];
 
 export const generate = async () => {
     logger.log(`Starting r{clr,cyanBright,${tasks.length}} tasks...`);
